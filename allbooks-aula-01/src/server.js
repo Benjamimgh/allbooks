bb1f80652615346f9e3e26370ed934f3ca3c15f3
unknown <hoppe.benjamim@hotmail.com>
2026-06-24 11:17:05 -0300

    Altera o Server

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do Allbooks")
   console.log("API disponível através da URL http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 8e2902da2ff5d2937f94ab92407b5713c1de9d4d
 })
